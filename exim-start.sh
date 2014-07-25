755b5308ff4f0496cc1d573e10c8cdeeb50b9cc6
Devon Weller <dweller@devonweller.com>
2014-07-25 16:06:49 -0500

    simplified exim start script

--- exim-start.sh
+++ exim-start.sh
@@ -1,24 +1,13 @@
 #!/bin/bash
 
-# get ec2 public hostname
-cat /etc/issue | grep Amazon
-if [ -e /etc/issue ]; then
-    AMAZON_ISSUE=$(cat /etc/issue | grep Amazon)
-    if [ -n "$AMAZON_ISSUE" ]; then
-        # is amazon
-        PUBLIC_HOSTNAME=$(curl -q http://169.254.169.254/latest/meta-data/public-hostname 2>/dev/null)
-    else
-        # not amazon
-        if [ -n "$HOST_DOMAIN" ]; then
-            # use HOST_DOMAIN environment variable
-            PUBLIC_HOSTNAME=$HOST_DOMAIN
-        else
-            # last resort: just use the IP address
-            PUBLIC_HOSTNAME=$(hostname -I | cut -f1 -d' ')
-        fi
-    fi
-fi;
-
+# get hostname
+if [ -n "$HOST_DOMAIN" ]; then
+    # use HOST_DOMAIN environment variable
+    PUBLIC_HOSTNAME=$HOST_DOMAIN
+else
+    # last resort: just use the IP address
+    PUBLIC_HOSTNAME=$(hostname -I | cut -f1 -d' ')
+fi
 
 # modify exim.conf
 SRC_FILE="/etc/exim/exim.conf"
